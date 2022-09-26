9bc9b4067bf8d07a2dadc0bbe8d06e26e27b17ec
mulwamark <markmulwa@gmail.com>
2022-09-26 06:29:33 +0300

    list views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import post
+from django.views.generic import ListView, DetailView
 
 
 
@@ -11,6 +12,15 @@ def home(request):
 	}
 	return render(request,'blog/home.html', context)
 
+class PostListView(ListView):
+	model = post
+	template_name = 'blog/home.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+	model = post
+	
 
 def about(request):
 	return render(request,'blog/about.html',{'title': 'About'})
